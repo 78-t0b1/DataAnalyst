76bd9f771c226cca118393919e14df8bf91ac719
78-t0b1 <tanmayjagtap78@gmail.com>
2024-10-13 02:00:17 +0100

    pORT ISSUES

--- FastAPI_Wrapper/server.py
+++ FastAPI_Wrapper/server.py
@@ -57,4 +57,6 @@ async def chat(request: MessageRequest):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+    
